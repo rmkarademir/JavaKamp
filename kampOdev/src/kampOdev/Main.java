7f6b81f76f60df261ccde58b8e1166fcf9da176e
Rüstem Karademir <rustemkarademir@gmail.com>
2021-05-04 13:15:04 +0300

    Odev

--- kampOdev/src/kampOdev/Main.java
+++ kampOdev/src/kampOdev/Main.java
@@ -4,18 +4,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		Lesson lesson1 = new Lesson();
-		Lesson lesson2 = new Lesson();
+		Lesson lesson2 = new Lesson(2, "Yazýlým Geliþtirici Yetiþtirme Kampý (JAVA + REACT)", "Engin Demirog", 25);
 		
 		lesson1.lessonId = 1;
 		lesson1.lessonName = "Yazýlým Geliþtirici Yetiþtirme Kampý (C# + ANGULAR)";
 		lesson1.instructorName = "Engin Demirog";
 		lesson1.percentageCompletion = 100;
 		
-		lesson2.lessonId = 2;
-		lesson2.lessonName = "Yazýlým Geliþtirici Yetiþtirme Kampý (JAVA + REACT)";
-		lesson2.instructorName = "Engin Demirog";
-		lesson2.percentageCompletion = 25;
+		Lesson[] lessons = {lesson1,lesson2};
 		
+		for(Lesson lesson:lessons) {
+			System.out.println("Ders adý: "+lesson.lessonName);
+			System.out.println("Eðitmen adý: "+lesson.instructorName);
+			System.out.println("Tamamlanma oraný: %"+lesson.percentageCompletion);
+			System.out.println("*********");
+
+		}
+		
+		ButtonManager buttonManager = new ButtonManager();
+		
+		buttonManager.submitButton();
+		buttonManager.nextButton();
+		buttonManager.previousButton();
 
 	}
 
